de30dd037bafaf18d5982ed3a2c73bf7dc6981b4
Jian Xi Chen <jianxi.chen@stonybrook.edu>
2020-10-16 19:54:48 -0400

    scheduler app added

---
+++ 316hw4react/src/scheduler.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TestApp extends Component {
+    state = { }
+    render() {
+        return(
+            <h1>Test</h1>
+        );
+    }
+}
+export default TestApp;
