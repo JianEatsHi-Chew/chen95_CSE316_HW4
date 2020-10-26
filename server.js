ebed5633ecbb5628e7603197d30d6e4dad8ceeb0
Jian Xi Chen <jianxi.chen@stonybrook.edu>
2020-10-25 23:20:35 -0400

    added mysql readibility

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
 var express = require("express");
 bodyParser = require("body-parser");
 var path = require("path");
-app = express();
-app.use(bodyParser.urlencoded({ extended: true })); //app.use(express.bodyParser()); works too
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.post("/", (req, res) => {
-    res.send(`Full name is:${req.body.fname}.`);
-});
-
-const port = 8080;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-
-/*------------MYSQL-------------*/
-
 const mysql = require("mysql");
-// ./node_modules/.bin/nodemon scripts.js
+// ./node_modules/.bin/nodemon server.js
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -30,24 +10,55 @@ const con = mysql.createConnection({
     password: "LEnovothemacv",
     database: "316hw4",
 });
+app = express();
+app.use(bodyParser.urlencoded({ extended: true })); //app.use(express.bodyParser()); works too
+const port = 8080;
+labels = [
+    "Subj",
+    "CRS",
+    "Title",
+    "Cmp",
+    "Sctn",
+    "Days",
+    "Start Time",
+    "End Time",
+    "Mtg Start Date",
+    "Mtg End Date",
+    "Duration",
+    "Instruction Mode",
+    "Building",
+    "Room",
+    "Instr",
+    "Enrl Cap",
+    "Wait Cap",
+    "Cmbnd Descr",
+    "Cmbnd Enrl Cap",
+];
 
-con.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    //console.log("Connection established");
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.post("/search", (req, res) => {
+    con.query(
+        `SELECT *
+        FROM courses 
+        WHERE ${req.body.fields} = ${req.body.search}`,
+        function (err, result) {
+            if(err) throw err;
+            string = "<table>";
+            for(i = 0; i < result.length; i++){
+                innerstring = "";
+                for(x = 0; x < labels.length; x++){
+                    innerstring += result[i][labels[x]] + "  ";
+                }
+                string += innerstring + "<br>";
+            }
+            res.send(`Searching:. ${string}`);
+        }
+    );
 });
 
-con.query("SELECT * FROM courses", (err, rows) => {
-    if (err) throw err;
-    //console.log("Data received from Db:");
-    //console.log(rows);
-});
-
-con.end((err) => {
-    // The connection is terminated gracefully
-    // Ensures all remaining queries are executed
-    // Then sends a quit packet to the MySQL server.
-    //console.log("Connection ended");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
